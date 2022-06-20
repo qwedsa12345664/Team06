1d09511ccbd8b29b375dd2549b954c70e4c82ac5
黃鈺凱 <qwedsa12345664@gmail.com>
2022-06-20 14:56:35 +0800

    update log function function -> .setTitle("結果:傲慢的孤獨者") function -> .setTitle("結果:吸引人的冒險家") function -> .setTitle("結果:以牙還牙的自我保護者") function -> .setTitle("結果:缺乏信心的挑惕者") function -> .setTitle("結果:內傾性格") function -> .setTitle("結果:生活狀況") function -> .setTitle("結果:情感狀況") function -> .setTitle("結果:財務狀況") function -> .setTitle("結果:工作狀況") function -> .setTitle("結果:身體狀況")

--- app/src/main/java/com/example/one/Page3.java
+++ app/src/main/java/com/example/one/Page3.java
@@ -57,35 +57,35 @@ public class Page3 extends AppCompatActivity {
                     if (score > 33) {
                         Toast.makeText(Page3.this, "生活狀況", Toast.LENGTH_SHORT).show();
                         new AlertDialog.Builder(Page3.this)
-                                .setTitle("結果")
+                                .setTitle("結果:生活狀況")
                                 .setMessage("你喜歡思考、富有哲學家的精神，無法忍受漫無目的地一天混過一天，你希望自己生命中的每一分、每一秒都是有意義的，所以你永遠都在問自己：「我的下一步要做什麼？」。")
                                 .show();
                         k = 1;
                     } else if (score >= 32) {
                         Toast.makeText(Page3.this, "情感狀況", Toast.LENGTH_SHORT).show();
                         new AlertDialog.Builder(Page3.this)
-                                .setTitle("結果")
+                                .setTitle("結果:情感狀況")
                                 .setMessage("你對「情」是很看重的，而且無論親情、愛情、友情都一樣，缺錢對你來說並不是什麼大問題，因為你淡泊名利，但是沒有「情」的日子你絕對過不下去，對每個人都會真心付出。")
                                 .show();
                         k = 1;
                     } else if (score >= 24) {
                         Toast.makeText(Page3.this, "財務狀況", Toast.LENGTH_SHORT).show();
                         new AlertDialog.Builder(Page3.this)
-                                .setTitle("結果")
+                                .setTitle("結果:財務狀況")
                                 .setMessage("你的腦子裡有無數個夢想，所以你希望在很短時間裡得到一大筆錢，無論是自己辛苦賺的或天上掉下來的都好，如此才能讓你立刻放下一切，毫無後顧之憂地去做自己想做的事。")
                                 .show();
                         k = 1;
                     } else if (score >= 16) {
                         Toast.makeText(Page3.this, "工作狀況", Toast.LENGTH_SHORT).show();
                         new AlertDialog.Builder(Page3.this)
-                                .setTitle("結果")
+                                .setTitle("結果:工作狀況")
                                 .setMessage("無論別人覺得你的工作表現好不好，其實只有你最清楚自己的工作狀況，有一些難以突破的障礙一直困擾著你，或許你該找一位值得信任的長輩商討對策，別再自我設限了。")
                                 .show();
                         k = 1;
                     } else if (score >= 8) {
                         Toast.makeText(Page3.this, "身體狀況", Toast.LENGTH_SHORT).show();
                         new AlertDialog.Builder(Page3.this)
-                                .setTitle("結果")
+                                .setTitle("結果:身體狀況")
                                 .setMessage("長期疲勞和精神壓力，讓你的身體發出或大或小的各種警訊，例如莫名的頭痛或渾身不對勁，你必須嚴肅地看待它們，並儘快找出解決之道，否則惡化速度會出乎你的想像。")
                                 .show();
                         k =  1;
